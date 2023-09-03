84370b8f5c510a30fc6a08021ab95ad7981ee0e6
Karol Gutkowski <01171298@pw.edu.pl>
2023-09-03 20:41:41 +0200

    added swagger to development enviroment

--- ExampleAPI/Program.cs
+++ ExampleAPI/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<PostDbContext>(options =>
 {
@@ -13,6 +14,16 @@ builder.Services.AddDbContext<PostDbContext>(options =>
 builder.Services.AddScoped<IPostRepository, PostRepository>();
 
 var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Example API");
+        c.RoutePrefix = string.Empty;
+    });
+}
 
 app.MapDefaultControllerRoute();
 
