0a3279d64fcf1e89bf8ca3468377bf905134ce3b
Karol Gutkowski <01171298@pw.edu.pl>
2023-09-03 18:38:30 +0200

    Initial api request

---
+++ ExampleAPI/Controllers/Api/PostsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleAPI.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        public IActionResult GetAll()
+        {
+            return Ok("test");
+        }
+    }
+}
