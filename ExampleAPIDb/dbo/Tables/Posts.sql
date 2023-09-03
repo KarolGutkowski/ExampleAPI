146d504e8c90eef737f38da4eae8d7df5f244b8f
Karol Gutkowski <01171298@pw.edu.pl>
2023-09-03 19:49:27 +0200

    imported database from sql server to local project

---
+++ ExampleAPIDb/dbo/Tables/Posts.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Posts] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Title]    NVARCHAR (MAX) NOT NULL,
+    [Content]  NVARCHAR (MAX) NOT NULL,
+    [AuthorId] INT            NOT NULL,
+    CONSTRAINT [PK_Posts] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Posts_Users_AuthorId] FOREIGN KEY ([AuthorId]) REFERENCES [dbo].[Users] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Posts_AuthorId]
+    ON [dbo].[Posts]([AuthorId] ASC);
+
---
+++ ExampleAPIDb/dbo/Tables/Users.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
