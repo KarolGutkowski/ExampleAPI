126b469fb924a5e3e1d8a9306b9fb6a329fe079a
Karol Gutkowski <01171298@pw.edu.pl>
2023-09-03 19:48:48 +0200

    injection of DB cotext for posts database

--- ExampleAPI/Program.cs
+++ ExampleAPI/Program.cs
@@ -1,5 +1,16 @@
+using ExampleAPI.Models;
+using ExampleAPI.Models.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<PostDbContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration["ConnectionStrings:ExampleAPIDBContextConnection"]);
+});
+builder.Services.AddScoped<IPostRepository, PostRepository>();
 
 var app = builder.Build();
 
