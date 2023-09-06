48167dacf794d23925da9ee451c0088593ab6daa
Karol Gutkowski <01171298@pw.edu.pl>
2023-09-06 18:29:31 +0200

    Revert "Revert "defined and created user repository""
    This reverts commit 2c46cf9149eac40a4b0de9bdba2cbbe7312ad545.

---
+++ ExampleAPI/Models/Repositories/Interfaces/IPostRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace ExampleAPI.Models.Repositories;
+
+public interface IPostRepository
+{
+    IEnumerable<Post> AllPosts { get; }
+    Post GetPostById(int id);
+    void AddPost(Post post);
+}
+
